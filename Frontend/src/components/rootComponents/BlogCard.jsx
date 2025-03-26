98bd8f76e70780c614882a4af004e273d4d220d9
Akshai Dev <akshaimuraleedharan4@gmail.com>
2025-03-26 18:32:03 +0000

    update blog card

--- Frontend/src/components/rootComponents/BlogCard.jsx
+++ Frontend/src/components/rootComponents/BlogCard.jsx
@@ -6,7 +6,11 @@
   return (
     <>
     <h1 className='text-md md:text-2xl dark:text-dark-heads text-black'>Latest Post</h1>
+<<<<<<< HEAD
           <div className='flex flex-col  relative md:flex-row md:justify-between lg:justify-evenly gap-5  mx-auto mt-5'> 
+=======
+          <div className='flex flex-col  md:flex-row md:justify-between lg:justify-evenly gap-5  mx-auto mt-5'> 
+>>>>>>> 38b743bbf6d763fbd0360fd6f6efa8e7b0c4e1c4
           <div className={`w-full md:w-[60%]  flex flex-col ${dataFetch.length === 0 ? "justify-center": ""}`}>
           { dataFetch.length === 0  ? <p className='flex justify-center'><span className="loading loading-spinner dark:bg-dark-spinners-color bg-black loading-sm"></span></p> : <SingleBlogCard data={dataFetch} hasMore={hasMore} fetchData={fetchData}/>}
           </div>
